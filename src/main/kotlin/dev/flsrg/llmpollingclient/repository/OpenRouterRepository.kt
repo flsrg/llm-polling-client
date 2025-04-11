b2748ed4bf8d191e0d1fb400214ce7341dd3e395
Maxim Shcherbakov <maksimkachentr228@gmail.com>
2025-04-11 10:26:29 +0700

    Fix the hist saving

--- src/main/kotlin/dev/flsrg/llmpollingclient/repository/OpenRouterRepository.kt
+++ src/main/kotlin/dev/flsrg/llmpollingclient/repository/OpenRouterRepository.kt
@@ -33,10 +33,10 @@ class OpenRouterRepository(private val api: Api): Repository {
                     val line = channel.readUTF8Line() ?: break
 
                     if (line.startsWith("data: ")) {
-                        validate(line)
 
                         val json = line.removePrefix("data: ").trim()
                         if (json == "[DONE]") break
+                        validate(json)
 
                         emit(transform(json))
                     }
@@ -48,7 +48,7 @@ class OpenRouterRepository(private val api: Api): Repository {
     private fun validate(json: String) {
         if (json.contains("error")) {
             val error = Json.parseToJsonElement(json).jsonObject["error"]
-            throw Exception("API error: $error")
+            if (error != null) throw Exception("API error: $error")
         }
     }
 }
